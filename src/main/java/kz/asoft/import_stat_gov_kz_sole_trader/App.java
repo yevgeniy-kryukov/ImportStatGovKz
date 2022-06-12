7a72c8e88afbff7f12c06e2b67c67069349757a1
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2022-06-12 18:08:05 +0600

    first commit

---
+++ src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/App.java
@@ -0,0 +1,13 @@
+package kz.asoft.import_stat_gov_kz_sole_trader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
