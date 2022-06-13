3a60f9fb12dc7df804ae22db78a68bbc278ce800
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2022-06-13 18:30:43 +0600

    4

--- src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/FileDownloader.java
+++ src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/FileDownloader.java
@@ -8,61 +8,71 @@ public class FileDownloader {
 
     public void getFile(int typeLegalUnitId) {
 
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("10.1.20.15", 8888));
+        try {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("10.1.20.15", 8888));
 
-        // Получение списка срезов
-        String jsonString = new HttpUtility().get("https://stat.gov.kz/api/rcut/ru", proxy);
-
-        // Находим идентификатор последнего среза
-        int lastCutId = -1;
-        JSONArray ja = new JSONArray(jsonString.toString());
-        for (int i = 0; i < ja.length(); i++)
-        {
-            int id = ja.getJSONObject(i).getInt("id");
-            if (id > lastCutId) {
-                lastCutId = id;
-            }
-        }
-
-        // Отправление запроса на выборку и получение номера заявки
-        String input = "{\"conditions\":[" +
-                "        {\"classVersionId\":2153,\"itemIds\":[" + typeLegalUnitId + "]}," +
-                        "{\"classVersionId\":1989,\"itemIds\":[39354,39355,39356,39358,534829,39359]}" +
-                        "]," +
-                        "\"cutId\":" + lastCutId + ",\"stringForMD5\":\"string\"}";
-        String jsonString2 = new HttpUtility().post("https://stat.gov.kz/api/sbr/request/?api", input, proxy);
-        JSONObject jo2 = new JSONObject(jsonString2);
-        if (!jo2.getBoolean("success")) {
-            System.out.println("Ошибка! Запрос на выборку не обработан");
-            return;
-        }
-        String objNumber =  jo2.getString("obj");
-
-        //  Проверка статуса заявки
-        String jsonString3 = "";
-        JSONObject jo3 = null;
-        String fileGuid = "";
-        for(int i=1; i <= 5; i++) {
-            jsonString3 = new HttpUtility().get("https://stat.gov.kz/api/sbr/requestResult/" + objNumber + "/ru", proxy);
-            jo3 = new JSONObject(jsonString3);
-
-            if (!jo3.getBoolean("success")) {
-                System.out.println("Ошибка! Не возможно проверить статус заявки");
+            // Получение списка срезов
+            String jsonString = new HttpUtility().get("https://stat.gov.kz/api/rcut/ru", proxy);
+            if (jsonString == null) {
+                System.out.println("Ошибка! Не удалось получить список срезов");
                 return;
             }
 
-            if (jo3.getString("description").equals("Обработан")) {
-                fileGuid = jo3.getJSONObject("obj").getString("fileGuid");
-                break;
+            // Находим идентификатор последнего среза
+            int lastCutId = -1;
+            JSONArray ja = new JSONArray(jsonString.toString());
+            for (int i = 0; i < ja.length(); i++) {
+                int id = ja.getJSONObject(i).getInt("id");
+                if (id > lastCutId) {
+                    lastCutId = id;
+                }
             }
 
-            try {
-                Thread.sleep(60000);
-            } catch(InterruptedException e) {
-                System.out.println("An Excetion occured: " + e);
+            // Отправление запроса на выборку и получение номера заявки
+            String input = "{\"conditions\":[" +
+                    "        {\"classVersionId\":2153,\"itemIds\":[" + typeLegalUnitId + "]}," +
+                            "{\"classVersionId\":1989,\"itemIds\":[39354,39355,39356,39358,534829,39359]}" +
+                            "]," +
+                            "\"cutId\":" + lastCutId + ",\"stringForMD5\":\"string\"}";
+            String jsonString2 = new HttpUtility().post("https://stat.gov.kz/api/sbr/request/?api", input, proxy);
+            JSONObject jo2 = new JSONObject(jsonString2);
+            if (!jo2.getBoolean("success")) {
+                System.out.println("Ошибка! Запрос на выборку не обработан");
+                return;
             }
+            String objNumber = jo2.getString("obj");
+
+            //  Проверка статуса заявки
+            String jsonString3 = "";
+            JSONObject jo3 = null;
+            String fileGuid = "";
+            for (int i = 1; i <= 5; i++) {
+                jsonString3 = new HttpUtility().get("https://stat.gov.kz/api/sbr/requestResult/" + objNumber + "/ru", proxy);
+                jo3 = new JSONObject(jsonString3);
+
+                if (!jo3.getBoolean("success")) {
+                    System.out.println("Ошибка! Не возможно проверить статус заявки");
+                    return;
+                }
+
+                if (jo3.getString("description").equals("Обработан")) {
+                    fileGuid = jo3.getJSONObject("obj").getString("fileGuid");
+                    break;
+                }
+
+                Thread.sleep(60000);
+            }
+            if (fileGuid.isEmpty()) {
+                System.out.println("Ошибка! Время выделенное на проверку статуса заявки окончено");
+                return;
+            }
+            System.out.println("fileGuid " + fileGuid);
+
+            // Загрузка zip-архива среза
+            new HttpUtility().downloadFile("https://stat.gov.kz/api/sbr/download?bucket=SBR_UREQUEST&guid=" + fileGuid, "c:\\windows\\temp", proxy);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
         }
-        System.out.println("fileGuid " + fileGuid);
 
     }
 }
--- src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/HttpUtility.java
+++ src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/HttpUtility.java
@@ -1,15 +1,11 @@
 package kz.asoft.import_stat_gov_kz_sole_trader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-import org.json.*;
-import java.io.OutputStream;
 
 public class HttpUtility {
-    public String get(String strURL, Proxy proxy) {
+    public String get(String strURL, Proxy proxy) throws IOException  {
         try {
             // Получение списка срезов
             URL url = new URL(strURL);
@@ -45,7 +41,7 @@ public class HttpUtility {
         return null;
     }
 
-    public String post(String strURL, String strParam, Proxy proxy) {
+    public String post(String strURL, String strParam, Proxy proxy) throws IOException  {
         try {
             URL url = new URL(strURL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection(proxy);
@@ -83,5 +79,66 @@ public class HttpUtility {
 
         }
         return null;
+    }
+
+    /**
+     * Downloads a file from a URL
+     * @param fileURL HTTP URL of the file to be downloaded
+     * @param saveDir path of the directory to save the file
+     * @param proxy
+     * @throws IOException
+     */
+    public void downloadFile(String fileURL, String saveDir, Proxy proxy) throws IOException {
+        URL url = new URL(fileURL);
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection(proxy);
+        int responseCode = httpConn.getResponseCode();
+
+        // always check HTTP response code first
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            String fileName = "";
+            String disposition = httpConn.getHeaderField("Content-Disposition");
+            String contentType = httpConn.getContentType();
+            int contentLength = httpConn.getContentLength();
+
+            if (disposition != null) {
+                // extracts file name from header field
+                int index = disposition.indexOf("filename=");
+                if (index > 0) {
+                    fileName = disposition.substring(index + 10,
+                            disposition.length() - 1);
+                }
+            } else {
+                // extracts file name from URL
+                fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
+                        fileURL.length());
+            }
+
+            System.out.println("Content-Type = " + contentType);
+            System.out.println("Content-Disposition = " + disposition);
+            System.out.println("Content-Length = " + contentLength);
+            System.out.println("fileName = " + fileName);
+
+            // opens input stream from the HTTP connection
+            InputStream inputStream = httpConn.getInputStream();
+            String saveFilePath = saveDir + File.separator + fileName;
+
+            // opens an output stream to save into file
+            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+            int bytesRead = -1;
+            final int BUFFER_SIZE = 4096;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+            System.out.println("File downloaded");
+        } else {
+            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+        }
+        httpConn.disconnect();
     }
 }
