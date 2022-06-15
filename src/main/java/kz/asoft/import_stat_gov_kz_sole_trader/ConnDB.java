b8c4c9754b79237aa4e7da3694ad510a53ce9858
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2022-06-15 18:50:16 +0600

    8

---
+++ src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/ConnDB.java
@@ -0,0 +1,25 @@
+package kz.asoft.import_stat_gov_kz_sole_trader;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.InputStream;
+import java.util.Properties;
+
+class ConnDB {
+    private ConnDB() {}
+
+    static Connection getConnection() throws Exception {
+        Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance();
+        final Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("database.properties"))) {
+            props.load(in);
+        }
+        final String url = props.getProperty("url");
+        final String username = props.getProperty("username");
+        final String password = props.getProperty("password");
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
--- src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/ExcelDataLoader.java
+++ src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/ExcelDataLoader.java
@@ -9,9 +9,35 @@ import com.monitorjbl.xlsx.StreamingReader;
 import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class ExcelDataLoader {
     public void loadDataFile(String fileLocation) throws Exception {
+        final Connection conn = ConnDB.getConnection();
+
+        final String sqlText = "INSERT INTO stat_gov_kz.g_legal (" +
+                                "bin_iin," +
+                                "full_name_kz," +
+                                "full_name," +
+                                "date_reg," +
+                                "oked_main_code," +
+                                "oked_main_activity_name_kz," +
+                                "oked_main_activity_name," +
+                                "secondary_oked_code_list," +
+                                "krp_code," +
+                                "krp_name_kz," +
+                                "krp_name," +
+                                "kato_code," +
+                                "locality_name_kz," +
+                                "locality_name," +
+                                "legal_address," +
+                                "leader_name," +
+                                "date_cut" +
+                                ") " +
+                                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        final PreparedStatement preparedStatement = conn.prepareStatement(sqlText);
+
         org.apache.log4j.PropertyConfigurator.configure("log4j.properties");
 
         InputStream is = Files.newInputStream(new File(fileLocation).toPath());
@@ -20,21 +46,31 @@ public class ExcelDataLoader {
                 .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
                 .open(is);            // InputStream or File for XLSX file (required)
 
-        int i = 0;
         for (Sheet sheet : workbook){
-            i = 0;
             System.out.println(sheet.getSheetName());
+            rowloop:
             for (Row r : sheet) {
-                i++;
+//                if (r.getRowNum() == 10) {
+//                    return;
+//                }
                 for (Cell c : r) {
-                    System.out.println(c.getStringCellValue());
+                    if (c.getColumnIndex() == 0) {
+                        try {
+                            long cellValue = Long.parseLong(c.getStringCellValue());
+                        } catch (NumberFormatException nfe) {
+                            continue rowloop;
+                        }
+                    }
+                    //System.out.println(c.getStringCellValue());
+                    //System.out.println(c.getColumnIndex() + 1);
+                    //System.out.println(c.getStringCellValue());
+                    preparedStatement.setString(c.getColumnIndex() + 1, c.getStringCellValue());
                 }
-
-                if (i==10) {
-                   break;
-                }
-
+                preparedStatement.setString(17, "01.06.2022");
+                final int rowsCount = preparedStatement.executeUpdate();
             }
         }
+        preparedStatement.close();
+        conn.close();
     }
 }
