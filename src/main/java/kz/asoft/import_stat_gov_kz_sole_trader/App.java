89ad084e1c34ce9d93c0f20033b81cf298c010f1
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2022-06-16 00:04:12 +0600

    11

--- src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/App.java
+++ src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/App.java
@@ -1,5 +1,12 @@
 package kz.asoft.import_stat_gov_kz_sole_trader;
 
+import org.json.JSONArray;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -13,18 +20,63 @@ public class App
 {
     public static void main(String[] args) {
         try {
-//            // считываем настройки
-//            final Properties props = new Properties();
-//            try(InputStream in = Files.newInputStream(Paths.get("app.properties"))) {
-//                props.load(in);
-//            }
-//            // скачиваем файл
-//            FileDownloader fd = new FileDownloader();
-//            String fileName = fd.getFile(742681, props.getProperty("downloadDir"));
-//            // разархивируем файл
-//            new UnzipUtility().unzip(props.getProperty("downloadDir") + "\\" + fileName, props.getProperty("downloadDir"));
+            // считываем настройки
+            final Properties props = new Properties();
+            try(InputStream in = Files.newInputStream(Paths.get("app.properties"))) {
+                props.load(in);
+            }
 
-            new ExcelDataLoader().loadDataFile("C:\\Windows\\Temp\\request-4286c2b12e6bbfdc4bc079caef7fc2ce.xlsx");
+            try (final Connection conn = ConnDB.getConnection()) {
+
+                Log log = new Log(conn);
+                if (log.isExistsUnfinishedProcess()) {
+                    return;
+                }
+
+                Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("10.1.20.15", 8888));
+
+                // Получение списка срезов
+                String jsonString = new HttpUtility().get("https://stat.gov.kz/api/rcut/ru", proxy);
+                if (jsonString == null) {
+                    throw new Exception("Ошибка! Не удалось получить список срезов");
+                }
+
+                // Находим идентификатор последнего среза
+                int lastCutId = -1;
+                String lastCutName = "";
+                JSONArray ja = new JSONArray(jsonString.toString());
+                for (int i = 0; i < ja.length(); i++) {
+                    int id = ja.getJSONObject(i).getInt("id");
+                    if (id > lastCutId) {
+                        lastCutId = id;
+                        lastCutName = ja.getJSONObject(i).getString("name");
+                    }
+                }
+
+                if (lastCutId == -1) {
+                    return;
+                }
+
+                if (!log.isExistsCut(lastCutId)) {
+                    log.addCut(lastCutId, lastCutName);
+                }
+
+                final String sqlText = "SELECT id FROM stat_gov_kz.d_type_legal_unit WHERE is_updated = true";
+                try(Statement statement = conn.createStatement();
+                    ResultSet resultSet = statement.executeQuery(sqlText);) {
+                    while (resultSet.next()) {
+                        log.startProcess(lastCutId, resultSet.getInt("id"));
+
+                        //            // скачиваем файл
+                        //            FileDownloader fd = new FileDownloader(proxy);
+                        //            String fileName = fd.getFile(lastCutId, 742681, props.getProperty("downloadDir"));
+                        //            // разархивируем файл
+                        //            new UnzipUtility().unzip(props.getProperty("downloadDir") + "\\" + fileName, props.getProperty("downloadDir"));
+
+                        new ExcelDataLoader().loadDataFile("C:\\Windows\\Temp\\request-4286c2b12e6bbfdc4bc079caef7fc2ce.xlsx");
+                    }
+                }
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/FileDownloader.java
+++ src/main/java/kz/asoft/import_stat_gov_kz_sole_trader/FileDownloader.java
@@ -4,33 +4,16 @@ import java.net.*;
 import org.json.*;
 
 public class FileDownloader {
+    private Proxy proxy;
 
-    public String getFile(int typeLegalUnitId, String downloadDir) throws Exception  {
-
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("10.1.20.15", 8888));
-
-        // Получение списка срезов
-        String jsonString = new HttpUtility().get("https://stat.gov.kz/api/rcut/ru", proxy);
-        if (jsonString == null) {
-            throw new Exception("Ошибка! Не удалось получить список срезов");
-        }
-
-        // Находим идентификатор последнего среза
-        int lastCutId = -1;
-        JSONArray ja = new JSONArray(jsonString.toString());
-        for (int i = 0; i < ja.length(); i++) {
-            int id = ja.getJSONObject(i).getInt("id");
-            if (id > lastCutId) {
-                lastCutId = id;
-            }
-        }
+    public String getFile(int cutId, int typeLegalUnitId, String downloadDir) throws Exception  {
 
         // Отправление запроса на выборку и получение номера заявки
         String input = "{\"conditions\":[" +
                 "        {\"classVersionId\":2153,\"itemIds\":[" + typeLegalUnitId + "]}," +
                         "{\"classVersionId\":1989,\"itemIds\":[39354,39355,39356,39358,534829,39359]}" +
                         "]," +
-                        "\"cutId\":" + lastCutId + ",\"stringForMD5\":\"string\"}";
+                        "\"cutId\":" + cutId + ",\"stringForMD5\":\"string\"}";
         String jsonString2 = new HttpUtility().post("https://stat.gov.kz/api/sbr/request/?api", input, proxy);
         JSONObject jo2 = new JSONObject(jsonString2);
         if (!jo2.getBoolean("success")) {
